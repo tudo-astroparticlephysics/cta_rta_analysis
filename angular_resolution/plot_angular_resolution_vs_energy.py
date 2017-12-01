871d4cbb2a3154afdd30e847e58377adb44c0902
Kai Bruegge <kai.bruegge@tu-dortmund.de>
2017-12-01 12:16:59 +0100

    add la plama update

--- angular_resolution/plot_angular_resolution_vs_energy.py
+++ angular_resolution/plot_angular_resolution_vs_energy.py
@@ -6,6 +6,8 @@ import astropy.units as u
 from astropy.coordinates import cartesian_to_spherical, Angle, SkyCoord, EarthLocation
 from dateutil import parser
 import seaborn as sns
+from scipy.stats import binned_statistic
+from matplotlib.colors import LogNorm
 
 
 @click.command()
@@ -48,17 +50,41 @@ def main(input_file, output_file):
     df['spherical_distance'] = c.separation(c_mc)
 
     df['mc:energy'] = df['mc:energy'].apply(lambda x: np.log10(x * 1000))
+    #
 
-    bins = np.linspace(df['mc:energy'].min(), df['mc:energy'].max(), 6)
-    # print(bins)
+    df = df[df['stereo:estimated_direction:z'] > 0]
+
+    bins = np.linspace(df['mc:energy'].min(), df['mc:energy'].max(), 10)
     df['energy_bin'] = pd.cut(df['mc:energy'], bins)
+    #
+    x = df['mc:energy'].values
+    #
+    y = df['spherical_distance'].values
+    #
 
+    bin_means, bin_edges, binnumber = binned_statistic(x, y, statistic='median', bins=25)
+    b_68, bin_edges, binnumber = binned_statistic(x, y, statistic=lambda y: np.percentile(y, 68), bins=25)
+    b_32, bin_edges, binnumber = binned_statistic(x, y, statistic=lambda y: np.percentile(y, 32), bins=25)
+
+    bin_width = (bin_edges[1] - bin_edges[0])
+    bin_centers = (bin_edges[1:] + bin_edges[0:-1])/2
+
+    # plt.errorbar(bin_centers, bin_means, yerr=[b_95, b_05], xerr=bin_width/2, elinewidth=2, lw=0, ecolor='#348ABD')
+    plt.scatter(x, y, s=0.2, color='#909090')
+    plt.hlines(bin_means, bin_edges[:-1], bin_edges[1:], lw=1, colors='#0a4f88')
+    c = bin_centers
+    c[0] = bin_edges.min()
+    c[-1] = bin_edges.max()
+    plt.fill_between(c, b_32, b_68, facecolor='#0a4f88', alpha=0.4,  interpolate=True)
+    # plt.hist2d(df['mc:energy'], df['spherical_distance'], bins=100, norm=LogNorm())
     # whis=[15.7, 15.7+68.2]
-    sns.boxplot(x='energy_bin', y='spherical_distance', data=df, fliersize=1, linewidth=1)
+    # sns.boxplot(x='energy_bin', y='spherical_distance', data=df, fliersize=0.5, linewidth=1)
+    plt.yscale('log')
     plt.ylabel('Distance to True Position / degree')
-    plt.ylabel('Distance to True Position / Degree')
-    plt.xlabel('Energy Bins / TeV')
-    plt.ylim([-0.08, 1])
+    plt.xlabel('log(Energy / TeV)')
+    plt.ylim([0, 0.8])
+    plt.xlim([bin_edges.min(), bin_edges.max()])
+
     plt.tight_layout()
     plt.savefig(output_file)
 
