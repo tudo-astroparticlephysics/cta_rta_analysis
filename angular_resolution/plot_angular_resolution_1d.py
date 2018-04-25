ce34067b6eaf4ef4cf318c47b6d212a88435df9e
Kai B <kai.bruegge@tu-dortmund.de>
2018-04-25 18:22:18 +0200

    more flexibel angular resolution plotting

--- angular_resolution/plot_angular_resolution_1d.py
+++ angular_resolution/plot_angular_resolution_1d.py
@@ -3,30 +3,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 import astropy.units as u
 from astropy.coordinates import Angle
+from astropy.coordinates.angle_utilities import angular_separation
 import fact.io
 
 
 @click.command()
-@click.argument('input_dl3_file', type=click.Path(exists=True))
+@click.argument('input_files', type=click.Path(exists=True), nargs=-1)
+@click.option('-l', '--label', multiple=True)
+@click.option('-c', '--color', multiple=True)
 @click.option('-o', '--output', type=click.Path(exists=False))
-@click.option('-b', '--bin_edges', nargs=2, help='the lowes and highest bin edges', type=float, default=(0, 5))
-def main(input_dl3_file, output, bin_edges):
-    df = fact.io.read_data(input_dl3_file, key='array_events').dropna()
+@click.option('-d', '--density', is_flag=True, default=False)
+@click.option('-t', '--title', default=None)
+def main(input_files, label, color, output, density, title):
+    if label and len(input_files) != len(label):
+        print('Must pass as many labels as gamma files as proton files')
 
-    alt = Angle(df.alt_prediction.values * u.rad).degree
-    mc_alt = Angle(df.mc_alt.values * u.rad).degree
+    if color and label and len(label) != len(color):
+        print('Must pass as many colors as labels')
 
-    az = Angle(df.az_prediction.values * u.rad).wrap_at(180 * u.deg).degree
-    mc_az = Angle(df.mc_az.values * u.rad).wrap_at(180 * u.deg).degree
+    if not label:
+        label = input_files
 
-    distance = np.sqrt((alt - mc_alt)**2 + (az - mc_az)**2)
-    resolution = np.percentile(distance, 68)
-    print(f'Plotting a total {len(df)} events')
+    if not color:
+        color = [None] * len(input_files)
 
-    plt.hist(distance, bins=np.linspace(*bin_edges, 100),)
-    plt.title('Gammas')
-    plt.axvline(resolution, color='gray', linestyle='--', label='0.68 percentile')
+    for input, l, c in zip(input_files, label, color):
+        df = fact.io.read_data(input, key='array_events').dropna()
+
+        mc_az = Angle(df.mc_az.values, unit=u.rad).wrap_at(180*u.deg)
+        mc_alt = Angle(df.mc_alt.values, unit=u.rad)
+
+        az = Angle(df.az_prediction.values, unit=u.rad).wrap_at(180*u.deg)
+        alt = Angle(df.alt_prediction.values, unit=u.rad)
+
+        distance = angular_separation(mc_az, mc_alt, az, alt).to('deg')
+        resolution = np.nanpercentile(distance, 68)
+        print(f'Plotting a total {len(df)} events')
+
+        plt.hist(distance, bins=np.linspace(0, np.nanpercentile(distance, 95), 100), label=l, color=c, histtype='step', lw=2.5, density=density)
+
+        plt.axvline(resolution, color='gray', linestyle='--', label='0.68 percentile')
+
     plt.xlabel('Distance between true and reco position')
+    if title:
+        plt.title(title)
     plt.legend()
 
     if output:
--- angular_resolution/plot_angular_resolution_vs_energy.py
+++ angular_resolution/plot_angular_resolution_vs_energy.py
@@ -11,7 +11,9 @@ import fact.io
 @click.command()
 @click.argument('input_dl3_file', type=click.Path(exists=True))
 @click.option('-o', '--output', type=click.Path(exists=False))
-def main(input_dl3_file, output):
+@click.option('-t', '--title', default=None)
+@click.option('-c', '--color', default='#0a4f88')
+def main(input_dl3_file, output, title, color):
     df = fact.io.read_data(input_dl3_file, key='array_events').dropna()
 
     alt = Angle(df.alt_prediction.values * u.rad).degree
@@ -36,18 +38,19 @@ def main(input_dl3_file, output):
     bin_centers = np.sqrt(bin_edges[1:] * bin_edges[:-1])
 
     plt.scatter(x, y, s=0.2, color='#909090')
-    plt.hlines(bin_means, bin_edges[:-1], bin_edges[1:], lw=1, colors='#0a4f88')
+    plt.hlines(bin_means, bin_edges[:-1], bin_edges[1:], lw=1, colors=color)
     c = bin_centers
     c[0] = bin_edges.min()
     c[-1] = bin_edges.max()
-    plt.fill_between(c, b_32, b_68, facecolor='#0a4f88', alpha=0.4, interpolate=False)
+    plt.fill_between(c, b_32, b_68, facecolor=color, alpha=0.4, interpolate=False)
     plt.xscale('log')
     plt.yscale('log')
     plt.ylabel('Distance to True Position / degree')
     plt.xlabel('Energy / TeV')
-    plt.ylim([0.001, 0.8])
+    plt.ylim([0.001, 100.8])
     plt.xlim([bin_edges.min(), bin_edges.max()])
-
+    if title:
+        plt.title(title)
     plt.tight_layout()
     if output:
         plt.savefig(output)
